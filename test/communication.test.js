866d61e6f919c34aaf269d25e1e16224c64cae21
Mark Spanbroek <mark@spanbroek.net>
2020-04-20 14:47:45 +0200

    Create user's key pairs in beforeEach()

--- test/communication.test.js
+++ test/communication.test.js
@@ -1,13 +1,15 @@
 import http from 'http'
 import Gun, { SEA } from 'gun'
 
-let server, peer1, peer2
+let server, peer1, peer2, user1, user2
 
-beforeEach(() => {
+beforeEach(async () => {
   server = http.createServer().listen(9876)
   Gun({web: server, radisk: false})
   peer1 = Gun('http://localhost:9876/gun')
   peer2 = Gun('http://localhost:9876/gun')
+  user1 = await SEA.pair()
+  user2 = await SEA.pair()
 })
 
 afterEach(async () => {
@@ -23,8 +25,6 @@ it('communicates', done => {
 })
 
 it('encrypts', async () => {
-  const user1 = await SEA.pair()
-  const user2 = await SEA.pair()
   const key1 = await SEA.secret(user2.epub, user1)
   const key2 = await SEA.secret(user1.epub, user2)
   const encrypted = await SEA.encrypt('hello', key1)
