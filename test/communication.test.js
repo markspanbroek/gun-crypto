64e4885e34543d2bfea8653b0ea37bb00b14d6ce
Mark Spanbroek <mark@spanbroek.net>
2020-04-20 14:21:24 +0200

    Setup two peers and a server peer, and test communication

--- test/communication.test.js
+++ test/communication.test.js
@@ -1,3 +1,25 @@
-it('tests', () => {
-  expect(true).toBeTruthy()
+import http from 'http'
+import Gun from 'gun'
+
+let server
+let gun
+
+beforeEach(() => {
+  server = http.createServer().listen(9876)
+  gun = Gun({web: server})
+})
+
+afterEach(async () => {
+  await server.close()
+})
+
+it('communicates', done => {
+  const peer1 = Gun('http://localhost:9876/gun')
+  const peer2 = Gun('http://localhost:9876/gun')
+
+  peer1.get('example').put({ message: 'hello'})
+  peer2.get('example').get('message').once(message => {
+    expect(message).toEqual('hello')
+    done()
+  })
 })
